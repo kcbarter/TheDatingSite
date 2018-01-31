42750bab3e51fad9600f5ff0d3315d2058129b22
Kevan Barter <Kbarter@mail.greenriver.edu>
2018-01-30 19:43:05 -0500

    updated validate and personal info

--- model/validate.php
+++ model/validate.php
@@ -1,1 +1,60 @@
 <?php
+
+function validFirstName($FirstName)
+{
+    if(!empty($FirstName) && ctype_alpha($FirstName))
+        return true;
+}
+
+function validLastName($LastName)
+{
+    if(!empty($LastName) && ctype_alpha($LastName))
+        return true;
+}
+
+function validAge($Age)
+{
+    if(!empty($Age) && $Age >= 18 && is_numeric($Age))
+        return true;
+}
+
+function validPhone($Phone)
+{
+    if(!empty($Phone) && strlen($Phone) == 10 && is_numeric($Phone))
+        return true;
+}
+
+function validGender($Gender)
+{
+    if(!empty($Gender))
+        return true;
+}
+
+$errors = array();
+
+if(!validFirstName($FirstName))
+{
+    $errors['FirstName'] = "Please enter a valid name!";
+}
+
+if(!validLastName($LastName))
+{
+    $errors['LastName'] = "Please enter a valid Last Name!";
+}
+
+if(!validAge($Age))
+{
+    $errors['Age'] = "Your not old enough to be on this site/Please enter an age!";
+}
+
+if(!validPhone($Phone))
+{
+    $errors['Phone'] = "Please enter a valid phone number";
+}
+
+if(!validGender($Gender))
+{
+    $errors['gender'] = "Please select a gender";
+}
+
+$success = (sizeof($errors) == 0);
